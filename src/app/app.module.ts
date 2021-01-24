fc7f05fa840b0538ef79c6add2433f06e0e306a5
Piotr Kania <piotr@przyklad.pl>
2021-01-24 11:58:29 +0100

    dodano modul contacts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactsModule } from './contacts/contacts.module';
 
 @NgModule({
   declarations: [
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    ContactsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
