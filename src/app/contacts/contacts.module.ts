3601d8762dcc98967103874c12fa8d1931ed0eaa
Piotr Kania <piotr@przyklad.pl>
2021-01-24 14:16:24 +0100

    dodano komponent reuzywalny header

--- src/app/contacts/contacts.module.ts
+++ src/app/contacts/contacts.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ContactsListComponent } from './contacts-list/contacts-list.component';
+import { SharedModule } from '../shared/shared.module';
 
 
 
 @NgModule({
   declarations: [ContactsListComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    SharedModule
   ],
   exports: [
     ContactsListComponent
